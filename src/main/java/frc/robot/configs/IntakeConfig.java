a72c42c1f19f24ba5570c5996e9e3efeeda485a9
edanliahovetsky <binding.lizards_0m@icloud.com>
2026-01-29 13:20:01 -0500

    Feat: intake pivot rework

--- src/main/java/frc/robot/configs/IntakeConfig.java
+++ src/main/java/frc/robot/configs/IntakeConfig.java
@@ -3,24 +3,48 @@ package frc.robot.configs;
 public class IntakeConfig {
     public String canBusName;
 
-    public int intakeCanId;
-    public boolean isIntakeInverted;
+    public int rollerCanId;
+    public boolean isRollerInverted;
 
-    public double intakeStatorCurrentLimit;
-    public double intakePeakForwardTorqueCurrent;
-    public double intakePeakReverseTorqueCurrent;
+    public double rollerStatorCurrentLimit;
+    public double rollerPeakForwardTorqueCurrent;
+    public double rollerPeakReverseTorqueCurrent;
 
-    public double intakeKS;
-    public double intakeKV;
-    public double intakeKA;
-    public double intakeKP;
-    public double intakeKI;
-    public double intakeKD;
+    public double rollerKS;
+    public double rollerKV;
+    public double rollerKA;
+    public double rollerKP;
+    public double rollerKI;
+    public double rollerKD;
 
-    public double intakeMotorToOutputShaftRatio;
+    public double rollerMotorToOutputShaftRatio;
 
     public double intakingVelocityRPS;
     public double outtakingVelocityRPS;
 
-    public double intakeVelocityToleranceRPS;
+    public double rollerVelocityToleranceRPS;
+
+    public int pivotCanId;
+    public boolean isPivotInverted;
+
+    public double pivotStatorCurrentLimit;
+    public double pivotPeakForwardTorqueCurrent;
+    public double pivotPeakReverseTorqueCurrent;
+
+    public double pivotKS;
+    public double pivotKV;
+    public double pivotKA;
+    public double pivotKP;
+    public double pivotKI;
+    public double pivotKD;
+
+    public double pivotMotorToOutputShaftRatio;
+
+    public double pivotStartingAngleRotations;
+    public double pivotMinAngleRotations;
+    public double pivotMaxAngleRotations;
+
+    public double pivotAngleToleranceRotations;
+    public double pivotUpAngleRotations;
+    public double pivotDownAngleRotations;
 }
