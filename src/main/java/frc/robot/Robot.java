cab8c930131f11d27810efef5a8f693a1a71a478
Sahil Gupta <sahil@guptainc.net>
2026-02-09 14:51:24 -0500

    manual override recommit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -128,7 +128,10 @@ public class Robot extends LoggedRobot {
         // robot's periodic
         // block in order for anything in the Command-based framework to work.
         CommandScheduler.getInstance().run();
+
+        // Dashboard updates
         Dashboard.updateData();
+        ManualOverride.update();
     }
 
     /** This function is called once each time the robot enters Disabled mode. */
